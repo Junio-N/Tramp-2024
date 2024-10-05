63183f00c6d214c746683391d0c1e2578f3f09a7
Junio-N <junio.novais.12@outlook.com>
2024-10-05 00:20:08 +0000

    Operações básicas com variáveis

--- Ola.c
+++ Ola.c
@@ -2,5 +2,22 @@
 #include <stdlib.h>
 
 void main () {
-    printf("Olá mundo!");
+    int x = 0;                                      //(criando uma variável Inteira)
+    printf("Valor de x: %d\n",x);                   //(Imprimindo um valor do tipo inteiro)
+    x++;                                            //(incrementando x em 1 unidade)
+    printf("Valor de x: %d\n",x);                   //(\n (espaçamento entre linhas) 
+    float y = 12.345678;                            //(criando uma variável Real)
+    printf("Valor de x: %f\n",y);                   //(Imprimindo o valor de y)
+    printf("Valor de x: %.2f\n",y);                 //(Imprimindo o valor de y com duas casas decimais)
+    char sexo = 'M';                                //(Variável que aceita 1 caracter [lembrando que tem que ser entre apóstrofes])
+    printf("Sexo: %c\n", sexo);                     //(Imprime a variável do tipo caracter)
+    char nome[30] = "joao";                         //(Criando vetor de caracteres para uma variável 'String' = Texto)
+    printf("Nome: %s\n", nome);                     //(Imprimindo um vetor de caracteres "String")
+    printf("Primeira Letra: %c\n", nome[0]);        //(Imprime o primeiro caracter do vetor que começa na posição 0)
+
+
+
+
+
+    printf("Olá Mundo!");  
 }
